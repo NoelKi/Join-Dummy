593d27db6fbab92844447a430ff953bb7e3e6b6f
Kieran Mai <kieran.noel@icloud.com>
2024-07-11 12:11:12 +0200

    add users

---
+++ js/contacts.js
@@ -0,0 +1,26 @@
+let user = [
+    {
+    'id': 0,
+        'name': 'Paul',
+        'surname': 'Blau',
+            'email': 'paul.blau@icloud.com'
+},
+{
+    'id': 1,
+        'name': 'Hans',
+        'surname': 'Gelb',
+            'email': 'hans.gelb@icloud.com'
+},
+{
+    'id': 2,
+        'name': 'Achim',
+        'surname': 'rot',
+            'email': 'achim.rot@icloud.com'
+},
+{
+    'id': 3,
+        'name': 'Johannes',
+        'surname': 'Grün',
+            'email': 'johannes.gruen@icloud.com'
+},
+]
