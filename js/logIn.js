b3d29d5e45fa443df3910db58becd82a3c5fd7d5
Alternie <neubert.marco@googlemail.com>
2024-07-13 10:37:19 +0200

    edit signUp

--- js/logIn.js
+++ js/logIn.js
@@ -1,3 +1,3 @@
 function changeToSignUp() {
-    window.location.href='../pages/signUp.html';
+    window.location.href = '../pages/signUp.html';
 }
--- js/signUp.js
+++ js/signUp.js
@@ -1,3 +1,18 @@
 function backToLogIn() {
-    window.location.href='../pages/login.html';
+    window.location.href = "login.html";
 }
+
+//const BASE_URL = "https://remotestorage-d1688-default-rtdb.europe-west1.firebasedatabase.app/"
+//
+//async function postSignUp(path = "", data = {}) {
+//    const response = await fetch(`${BASE_URL}${path}.json`, {
+//        method: "POST",
+//        headers: {
+//            "Content-Type": "application/json",
+//        },
+//        body: JSON.stringify(data),
+//    });
+//    const responseData = await response.json();
+//    console.log(responseData);
+//}
+
