8b1e077a1c19ed2afbbfd54469f272aadbefc2c4
Alternie <neubert.marco@googlemail.com>
2024-07-14 20:05:02 +0200

    js header

--- js/header.js
+++ js/header.js
@@ -5,15 +5,15 @@ function myFunction() {
   isPopupVisible = !isPopupVisible;
   popupNav.style.display = isPopupVisible ? "block" : "none";
   if (isPopupVisible) {
-    document.addEventListener('keydown', handleKeyPress);
-    document.addEventListener('click', handleClickOutside);
+    document.addEventListener('keydown', keyPress);
+    document.addEventListener('click', clickOutside);
   } else {
-    document.removeEventListener('keydown', handleKeyPress);
-    document.removeEventListener('click', handleClickOutside);
+    document.removeEventListener('keydown', keyPress);
+    document.removeEventListener('click', clickOutside);
   }
 }
 
-function handleKeyPress(e) {
+function keyPress(e) {
   switch (e.key) {
     case 'Escape':
       myFunction();
@@ -21,7 +21,7 @@ function handleKeyPress(e) {
   }
 }
 
-function handleClickOutside(event) {
+function clickOutside(event) {
   const popupNav = document.getElementById("popup-nav");
   const toggleNav = document.getElementById("toggle-nav");
   if (!popupNav.contains(event.target) && event.target !== toggleNav) {
