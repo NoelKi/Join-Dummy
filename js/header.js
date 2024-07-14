5efdae6518496486ca7998c074fd232f62e46651
Alternie <neubert.marco@googlemail.com>
2024-07-14 19:45:52 +0200

    js/html/css header

--- js/header.js
+++ js/header.js
@@ -1,33 +1,70 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleNav = document.getElementById("toggle-nav");
+function myFunction() {
   const popupNav = document.getElementById("popup-nav");
-
-  function togglePopupNav() {
-    if (popupNav.style.display === "none" || popupNav.style.display === "") {
-      popupNav.style.display = "block";
-    } else {
-      popupNav.style.display = "none";
-    }
+  if (popupNav.style.display === "block") {
+    popupNav.style.display = "none";
+    document.removeEventListener('keydown', handleKeyPress);
+    isPopupVisible = false;
+  } else {
+    popupNav.style.display = "block";
+    document.addEventListener('keydown', handleKeyPress);
+    isPopupVisible = true;
   }
+}
 
-  function closePopupNav(event) {
-    if (!popupNav.contains(event.target) && !toggleNav.contains(event.target)) {
-      popupNav.style.display = "none";
-    }
+function handleKeyPress(e) {
+  switch (e.key) {
+    case 'Escape':
+      closeNav();
+      break;
   }
+}
 
-  toggleNav.addEventListener("click", function (event) {
-    event.stopPropagation();
-    togglePopupNav();
-  });
+function closeNav() {
+  const popupNav = document.getElementById("popup-nav");
+  popupNav.style.display = "none";
+  isPopupVisible = false;
+}
 
-  document.addEventListener("click", function (event) {
-    closePopupNav(event);
-  });
-
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "Escape") {
-      popupNav.style.display = "none";
-    }
-  });
+document.addEventListener('click', function(event) {
+  const popupNav = document.getElementById("popup-nav");
+  const toggleNav = document.getElementById("toggle-nav");
+  if (isPopupVisible &&!popupNav.contains(event.target) && event.target!== toggleNav) {
+    closeNav();
+  }
 });
+
+
+//document.addEventListener("DOMContentLoaded", function () {
+//  const toggleNav = document.getElementById("toggle-nav");
+//  const popupNav = document.getElementById("popup-nav");
+//
+//  function togglePopupNav() {
+//    if (popupNav.style.display === "none" || popupNav.style.display === "") {
+//      popupNav.style.display = "block";
+//    } else {
+//      popupNav.style.display = "none";
+//    }
+//  }
+//
+//  function closePopupNav(event) {
+//    if (!popupNav.contains(event.target) && !toggleNav.contains(event.target)) {
+//      popupNav.style.display = "none";
+//    }
+//  }
+//
+//  toggleNav.addEventListener("click", function (event) {
+//    event.stopPropagation();
+//    togglePopupNav();
+//  });
+//
+//  document.addEventListener("click", function (event) {
+//    closePopupNav(event);
+//  });
+//
+//  document.addEventListener("keydown", function (event) {
+//    if (event.key === "Escape") {
+//      popupNav.style.display = "none";
+//    }
+//  });
+//});
+//
