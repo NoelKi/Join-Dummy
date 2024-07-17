1a745cdb93309b41016db7661d6dbc58b9b72516
Kieran Mai <kieran.noel@icloud.com>
2024-07-17 09:29:52 +0200

    add auth

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+USER_ID = '';
+
+function loadUserIdLocalStorage() {
+    const idAsText = localStorage.getItem('userId');
+    if (!idAsText) return window.location.href = 'login.html';
+    USER_ID = JSON.parse(idAsText);
+}
+
+loadUserIdLocalStorage();
