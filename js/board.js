bd4bad90e50f1db6fded9bdd158ef59e99b267df
Kieran Mai <kieran.noel@icloud.com>
2024-07-22 18:51:25 +0200

    add task card

--- js/board.js
+++ js/board.js
@@ -2,39 +2,54 @@ let currentDraggedElement;
 
 let tasks = [{
     'id': 0,
+    'date':'10/21/2024',
     'title': 'Putzen',
     'kind': 'Technical Task',
     'taskColor': 'blue',
     'description': 'BlaBliBulb',
     'category': 'inProgress',
-    'priority': 'medium',
+    'priority': 'Medium',
     'collaborators': [{'name':'Anche Apfelgrün',
                         'color':'#C3FF2B'},
                          {'name':'Rosi Rot',
-                         'color':'#FF4646'}]
+                         'color':'#FF4646'}],
+    'subtask': [{'name':'Küche putzen',
+                'state':'done',
+                'id': 2023},
+                {'name':'Flur putzen',
+                'state':'done',
+                'id': 206},]
 }, {
     'id': 1,
+    'date':'10/21/2024',
     'title': 'Kochen',
     'kind': 'User Story',
     'taskColor': 'orange',
     'description': 'BlaBliBulb',
     'category': 'toDo',
-    'priority': 'low',
+    'priority': 'Low',
     'collaborators': [{'name':'Anche Apfelgrün',
-                        'color':'#FF7A00'}]
+                        'color':'#FF7A00'}],
+    'subtask': [{'name':'Abendbrot kochen',
+                'state':'done',
+                'id': 2029},
+                {'name':'Frühstück kochen',
+                'state':'open',
+                'id': 2028},]
 }, {
     'id': 2,
+    'date':'10/21/2024',
     'title': 'Einkaufen',
     'kind': 'User Story',
     'taskColor': 'orange',
     'description': 'BlaBliBulb',
     'category': 'awaitFeedback',
-    'priority': 'high',
+    'priority': 'High',
     'collaborators': [{'name':'Anche Apfelgrün',
                         'color':'#1FD7C1'},
                         {'name':'Rosi Rot',
-                        'color':'#00BEE8'}]
-
+                        'color':'#00BEE8'}],
+    'subtask': []
 }];
 
 function renderTasks() {
@@ -127,7 +142,7 @@ function removeHighlight(id) {
 }
 
 function renderTaskHtml(element) {
-    let a = `<div class="task-container" draggable="true" ondragstart="startDragging(${element['id']})">
+    let a = `<div class="task-container" draggable="true" ondragstart="startDragging(${element['id']})" onclick="renderTaskOverlay(${element['id']});">
         <div class="task-kind-container" style="background-color: ${element.taskColor}">${element.kind}</div>
         <div class="task-content-container">
             <div class="task-title">${element.title}</div>
@@ -141,9 +156,7 @@ function renderTaskHtml(element) {
             <div class="task-bottom-container">
             <div class="task-collaborators" id="task-collaborators-${element.id}">`;
     for (const collab of element.collaborators) {
-        console.log(collab);
         [name, surname] = collab.name;
-
         initials = getFirstLetterOfName(name) + getFirstLetterOfName(surname);
         a += `<div class="initials" style="background-color: ${collab.color}">${initials}</div>`
     }
@@ -180,13 +193,13 @@ function closeOverlay() {
     content.style.display = 'none';
 }
 
-function openAddTaskOverlay() {
+function renderAddTaskOverlay() {
     const content = document.getElementById('board-overlay-section');
     content.style.display = 'block';
-    content.innerHTML = renderAddTaskOverlay();
+    content.innerHTML = createAddTaskOverlay();
 }
 
-function renderAddTaskOverlay() {   
+function createAddTaskOverlay() {   
     return `    
     <div class="board-overlay-section">
         <div class="board-overlay-container" id="board-overlay-container">
@@ -201,3 +214,86 @@ function renderAddTaskOverlay() {
     </div>`;
 }
 
+function renderTaskOverlay(id) {
+    const element = getObjectById(tasks,id);
+    const content = document.getElementById('board-overlay-section');
+    content.style.display = 'block';
+    content.innerHTML = createTaskOverlay(element);
+}
+
+function createTaskOverlay(element) {
+    let a = `<div class="show-task-overlay">
+        <div class="show-task-container">
+            <div class="top-task-container">
+                <div class="task-kind-container no-margin font-size-19" style="background-color: ${element.taskColor}">
+                    ${element.kind}
+                </div>
+                <button class="close-btn-overlay no-margin" onclick="closeOverlay()">
+                    <img src="../assets/img/closeTask.svg">
+                </button>
+            </div>
+            <div class="task-title-container">
+                ${element.title}
+            </div>
+            <div class="m-b-20">
+                ${element.description}
+            </div>
+            <div class="m-b-20">
+                <b>Due date:</b> &ensp; ${element.date}
+            </div>
+            <div class="task-overlay-priority">
+                <b>Priority:</b>&ensp;&ensp;&ensp; ${element.priority} <img src="../assets/img/priority${element.priority}.svg">
+            </div>`;
+    if (element.collaborators.length > 0) {
+            a += `<div>
+                <b>Assigned to:</b> <br>`;
+        for (const collab of element.collaborators) {
+            [name, surname] = collab.name;
+            initials = getFirstLetterOfName(name) + getFirstLetterOfName(surname);
+            a += `<div class="flex-row m-l-12 m-t-12"> 
+            <div class="initials m-r-10" style="background-color: ${collab.color}">${initials}</div> ${collab.name}
+            </div>`;
+        }
+        a += `</div>`;
+    }
+    if (element.subtask.length > 0) {
+        a += `<div class="task-subtask-container m-t-12" id="subtasks-overlay">
+                <b>Subtasks</b>
+            <div class="subtask-inner-container m-t-20">`;
+        for (const subtask of element.subtask) {
+            const name = subtask.name;
+            const state = subtask.state; 
+            const id = subtask.id;
+            a += `<div class="subtask-inner-inner-container">
+            <img src="../assets/img/${subtask.state}CheckButton.svg" onclick="switchSubtaskState(${element.id},${subtask.id})"> &ensp;${subtask.name}
+            </div>`
+        }
+        a += `</div>`;
+    } 
+    a += `</div>
+            <div class="task-edit-container">
+                <div class="delete-task-overlay-btn">
+                    <img src="../assets/img/bin.svg"> Delete 
+                </div>
+                <div class="horizontal-separator"></div>
+                <div class="edit-task-overlay-btn">
+                    <img src="../assets/img/pencil.svg"> Edit
+                </div>
+            </div>
+        </div>
+    </div>`;
+    return a;
+}
+
+function getObjectById(array, id) {
+    return array.find(obj => obj.id === id);
+}
+
+function switchSubtaskState(taskId,subtaskId) {
+    const element = getObjectById(tasks,taskId);
+    const subtask = getObjectById(element.subtask,subtaskId)
+    if (subtask.state === 'done') {
+        subtask.state = 'open';
+    } else {subtask.state = 'done'}
+    renderTaskOverlay(taskId);
+}
