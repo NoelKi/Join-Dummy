cd9b6812f8b3a2280464a9bd244cf97c5f63d7e8
Alternie <neubert.marco@googlemail.com>
2024-08-02 15:37:52 +0200

    add loadingScreenUser

--- js/logIn.js
+++ js/logIn.js
@@ -3,6 +3,7 @@ function fetchUsers(options = {}) {
     .then(response => response.json());
 }
 
+
 async function checkLogInData(email, password) {
   const users = await fetchUsers();
   if (!users) {
@@ -18,16 +19,66 @@ async function checkLogInData(email, password) {
   return false;
 }
 
+
 async function login() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   if (await checkLogInData(email, password)) {
-    window.location.href = 'index.html';
+    showLoadingScreen(() => window.location.href = 'index.html');
   } else {
     alert('Login failed: Incorrect email or password');
   }
-  getUserLists();
 }
+
+
+function getGreeting() {
+  const currentTime = new Date().getHours();
+  if (currentTime < 12) {
+    return "Good morning";
+  } else if (currentTime < 17) {
+    return "Good afternoon";
+  } else {
+    return "Good evening";
+  }
+}
+
+
+function showLoadingScreen(callback) {
+  updateGreetingMessage();
+  displayLoadingScreens();
+
+  setTimeout(() => {
+    hideLoadingScreens();
+    if (callback) callback();
+  }, 2000);
+}
+
+
+function displayLoadingScreens() {
+  const loadingScreen = document.getElementById('loading-screen');
+  const loadingScreenSummary = document.getElementById('loading-screen-summary');
+  const mainContent = document.getElementById('main-content');
+
+  loadingScreen.style.display = 'flex';
+  loadingScreenSummary.style.display = 'flex';
+  mainContent.style.display = 'none';
+}
+
+function hideLoadingScreens() {
+  const loadingScreen = document.getElementById('loading-screen');
+  const loadingScreenSummary = document.getElementById('loading-screen-summary');
+
+  loadingScreen.style.display = 'none';
+  loadingScreenSummary.style.display = 'none';
+}
+
+function updateGreetingMessage() {
+  const userName = JSON.parse(localStorage.getItem('userName')) || "Guest";
+  const greeting = getGreeting();
+
+  document.getElementById('greetingLoading').innerHTML = `${greeting}, &nbsp; <span>${userName}</span>`;
+}
+
 
 function saveUserIdLocalStorage(id, name) {
   const idAsText = JSON.stringify(id);
@@ -36,16 +87,19 @@ function saveUserIdLocalStorage(id, name) {
   localStorage.setItem('userName', nameAsText);
 }
 
+
 function loadUserIdLocalStorage() {
   const idAsText = localStorage.getItem('userId');
   if (!idAsText) return window.location.href = 'login.html';
   return JSON.parse(idAsText);
 }
 
+
 function toggleCheckBox() {
   let image = document.getElementById('checkbox-remember');
   image.src = image.src.includes('rememberDefault') ? '../assets/img/rememberChecked.svg' : '../assets/img/rememberDefault.svg';
 }
+
 
 function changeToSignUp() {
   window.location.href = '../pages/signUp.html';
