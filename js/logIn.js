576a5810b2ec47d74cbac40df97b390b09efd1b4
Alternie <neubert.marco@googlemail.com>
2024-08-06 16:17:24 +0200

    edit format

--- js/logIn.js
+++ js/logIn.js
@@ -1,5 +1,6 @@
 /**
  * Initializes the password inputs by adding event listeners.
+ * This function calls `initializePasswordInputs` to set up event listeners on the password input fields.
  * @function initi
  */
 function initi() {
@@ -9,6 +10,7 @@ function initi() {
 
 /**
  * Fetches the user data from the server.
+ * This function fetches the user data from the server using the provided options and returns the data as a JSON object.
  * @function fetchUsers
  * @param {Object} [options={}] - Optional fetch options.
  * @returns {Promise<Object>} The user data.
@@ -21,6 +23,8 @@ function fetchUsers(options = {}) {
 
 /**
  * Checks the login data against the stored user data.
+ * This function checks the provided email and password against the stored user data.
+ * If a match is found, it saves the user ID and name to localStorage.
  * @async
  * @function checkLogInData
  * @param {string} email - The user's email.
@@ -45,6 +49,9 @@ async function checkLogInData(email, password) {
 
 /**
  * Handles the login process.
+ * This function retrieves the email and password from the input fields,
+ * checks them against the stored user data, and redirects to the main page if successful.
+ * Otherwise, it shows an alert with a login failure message.
  * @async
  * @function login
  */
@@ -61,6 +68,7 @@ async function login() {
 
 /**
  * Handles the guest login process.
+ * This function logs in as a guest user and redirects to the main page.
  * @async
  * @function guestLogin
  */
@@ -72,6 +80,7 @@ async function guestLogin() {
 
 /**
  * Returns an appropriate greeting based on the current time.
+ * This function returns a greeting message based on the current time of day.
  * @function getGreeting
  * @returns {string} The greeting message.
  */
@@ -89,13 +98,13 @@ function getGreeting() {
 
 /**
  * Displays the loading screen and executes a callback after a delay.
+ * This function displays a loading screen, waits for a short delay, and then executes the provided callback function.
  * @function showLoadingScreen
  * @param {Function} callback - The callback function to execute after the delay.
  */
 function showLoadingScreen(callback) {
   updateGreetingMessage();
   displayLoadingScreens();
-
   setTimeout(() => {
     hideLoadingScreens();
     if (callback) callback();
@@ -105,13 +114,13 @@ function showLoadingScreen(callback) {
 
 /**
  * Displays the loading screens.
+ * This function makes the loading screens visible and hides the main content.
  * @function displayLoadingScreens
  */
 function displayLoadingScreens() {
   const loadingScreen = document.getElementById('loading-screen');
   const loadingScreenSummary = document.getElementById('loading-screen-summary');
   const mainContent = document.getElementById('main-content');
-
   loadingScreen.style.display = 'flex';
   loadingScreenSummary.style.display = 'flex';
   mainContent.style.display = 'none';
@@ -120,12 +129,12 @@ function displayLoadingScreens() {
 
 /**
  * Hides the loading screens.
+ * This function hides the loading screens and shows the main content.
  * @function hideLoadingScreens
  */
 function hideLoadingScreens() {
   const loadingScreen = document.getElementById('loading-screen');
   const loadingScreenSummary = document.getElementById('loading-screen-summary');
-
   loadingScreen.style.display = 'none';
   loadingScreenSummary.style.display = 'none';
 }
@@ -133,6 +142,7 @@ function hideLoadingScreens() {
 
 /**
  * Capitalizes the first letter of each word in a name.
+ * This function capitalizes the first letter of each word in the provided name.
  * @function capitalizeName
  * @param {string} name - The name to capitalize.
  * @returns {string} The capitalized name.
@@ -144,6 +154,7 @@ function capitalizeName(name) {
 
 /**
  * Capitalizes the first letter of a string.
+ * This function capitalizes the first letter of the provided string and converts the rest of the string to lowercase.
  * @function capitalizeFirstLetter
  * @param {string} string - The string to capitalize.
  * @returns {string} The string with the first letter capitalized.
@@ -155,6 +166,7 @@ function capitalizeFirstLetter(string) {
 
 /**
  * Updates the greeting message based on the user's name.
+ * This function updates the greeting message with the user's name if available, or uses "Guest" otherwise.
  * @function updateGreetingMessage
  */
 function updateGreetingMessage() {
@@ -170,6 +182,7 @@ function updateGreetingMessage() {
 
 /**
  * Saves the user ID and name to localStorage.
+ * This function saves the user ID and name to localStorage.
  * @function saveUserIdLocalStorage
  * @param {string} id - The user ID.
  * @param {string} name - The user name.
@@ -184,6 +197,7 @@ function saveUserIdLocalStorage(id, name) {
 
 /**
  * Loads the user ID from localStorage and redirects to login if not found.
+ * This function loads the user ID from localStorage. If no user ID is found, it redirects to the login page.
  * @function loadUserIdLocalStorage
  * @returns {string} The user ID.
  */
@@ -196,6 +210,7 @@ function loadUserIdLocalStorage() {
 
 /**
  * Toggles the checkbox image between checked and unchecked states.
+ * This function toggles the checkbox image between the checked and unchecked states.
  * @function toggleCheckBox
  */
 function toggleCheckBox() {
@@ -206,13 +221,15 @@ function toggleCheckBox() {
 
 /**
  * Handles the guest user login process.
+ * This function logs in as a guest user by looking for the guest email in the user database.
+ * If the guest user is found, it saves the user ID and name to localStorage.
+
  * @async
  * @function guestUser
  */
 async function guestUser() {
   const guestEmail = "guest@example.com";
   const users = await fetchUsers();
-
   for (const key in users) {
     const user = users[key];
     if (user.email === guestEmail) {
@@ -226,6 +243,7 @@ async function guestUser() {
 
 /**
  * Initializes the password input fields by adding event listeners.
+ * This function sets up event listeners on the password input fields to handle input events and toggle visibility.
  * @function initializePasswordInputs
  */
 function initializePasswordInputs() {
@@ -241,6 +259,7 @@ function initializePasswordInputs() {
 
 /**
  * Handles the password input event to show or hide the visibility icon.
+ * This function handles the input event on password fields, showing the appropriate icon based on the input value.
  * @function handlePasswordInput
  * @param {HTMLInputElement} passwordInput - The password input element.
  * @param {HTMLElement} passwordIcon - The password icon element.
@@ -257,6 +276,7 @@ function handlePasswordInput(passwordInput, passwordIcon, toggleVisibilityIcon) 
 
 /**
  * Toggles the visibility of the password input field.
+ * This function toggles the password input field between visible and hidden states.
  * @function togglePasswordVisibility
  * @param {HTMLInputElement} passwordInput - The password input element.
  * @param {HTMLElement} toggleVisibilityIcon - The toggle visibility icon element.
@@ -271,6 +291,7 @@ function togglePasswordVisibility(passwordInput, toggleVisibilityIcon) {
 
 /**
  * Shows the visibility icon and hides the password icon.
+ * This function shows the visibility icon and hides the password icon.
  * @function showVisibilityIcon
  * @param {HTMLElement} passwordIcon - The password icon element.
  * @param {HTMLElement} toggleVisibilityIcon - The toggle visibility icon element.
@@ -283,6 +304,7 @@ function showVisibilityIcon(passwordIcon, toggleVisibilityIcon) {
 
 /**
  * Shows the password icon and hides the visibility icon.
+ * This function shows the password icon and hides the visibility icon.
  * @function showPasswordIcon
  * @param {HTMLElement} passwordIcon - The password icon element.
  * @param {HTMLElement} toggleVisibilityIcon - The toggle visibility icon element.
@@ -295,6 +317,7 @@ function showPasswordIcon(passwordIcon, toggleVisibilityIcon) {
 
 /**
  * Redirects to the sign-up page.
+ * This function redirects the user to the sign-up page.
  * @function changeToSignUp
  */
 function changeToSignUp() {
