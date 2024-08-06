b0dcee307c5a403743ea5dfa99a8201664838378
Alternie <neubert.marco@googlemail.com>
2024-08-06 16:00:18 +0200

    edit format

--- js/summary.js
+++ js/summary.js
@@ -1,5 +1,7 @@
 /**
  * Initializes the application by including HTML components, retrieving user lists, and executing onload functions.
+ *  * This function serves as the entry point of the application. It initializes the necessary HTML components,
+ * retrieves user-specific data, and sets up the application state by calling `includeHTML`, `getUserLists`, and `onloadFunc`.
  * @function init
  */
 function init() {
@@ -11,6 +13,8 @@ function init() {
 
 /**
  * Fetches user data and initializes user-related information.
+ * This asynchronous function fetches user data from the server using the provided user ID.
+ * It then initializes user-specific data, such as tasks, and updates the application state.
  * @async
  * @function getUserLists
  * @throws Will throw an error if user data cannot be fetched.
@@ -31,6 +35,8 @@ async function getUserLists() {
 
 /**
  * Loads user ID and user name from local storage and updates the greeting message.
+ * This function retrieves the user ID and user name from local storage and updates the greeting message
+ * on the page based on the retrieved data. If the user data is not available, it defaults to greeting a 'Guest'.
  * @async
  * @function onloadFunc
  */
@@ -48,6 +54,8 @@ async function onloadFunc() {
 
 /**
  * Updates the greeting message on the page based on the current time and user name.
+ * This function updates the greeting message displayed on the page.
+ * The greeting changes based on the time of day (morning, afternoon, evening) and includes the user's name.
  * @function updateGreeting
  * @param {string} username - The name of the user to greet.
  */
@@ -72,6 +80,8 @@ function updateGreeting(username) {
 
 /**
  * Counts the number of tasks in each category and updates the corresponding elements on the page.
+ * This function categorizes and counts the tasks based on their status and updates the corresponding
+ * HTML elements with these counts.
  * @function countTasks
  * @param {Object[]} tasks - The list of tasks to count.
  */
@@ -91,6 +101,8 @@ function countTasks(tasks) {
 
 /**
  * Updates the urgent task information on the page.
+ * This function filters the tasks to identify those that are due within the next 7 days
+ * and updates the HTML elements to reflect the number of urgent tasks and the closest deadline.
  * @function updateUrgentTask
  * @param {Object[]} tasks - The list of tasks to check for urgency.
  */
@@ -110,6 +122,7 @@ function updateUrgentTask(tasks) {
 
 /**
  * Capitalizes the first letter of each word in a name.
+ * This function takes a name string and capitalizes the first letter of each word in the name.
  * @function capitalizeName
  * @param {string} name - The name to capitalize.
  * @returns {string} - The capitalized name.
@@ -121,6 +134,7 @@ function capitalizeName(name) {
 
 /**
  * Capitalizes the first letter of a string.
+ * This function capitalizes the first letter of the given string and converts the rest of the string to lowercase.
  * @function capitalizeFirstLetter
  * @param {string} string - The string to capitalize.
  * @returns {string} - The string with the first letter capitalized.
@@ -132,6 +146,7 @@ function capitalizeFirstLetter(string) {
 
 /**
  * Retrieves tasks with deadlines within the next 7 days.
+ * This function filters the given tasks and returns only those that are due within the next 7 days.
  * @function getTasksDueInNextSevenDays
  * @param {Object[]} tasks - The list of tasks to filter.
  * @returns {Object[]} - The list of tasks due in the next 7 days.
@@ -150,6 +165,7 @@ function getTasksDueInNextSevenDays(tasks) {
 
 /**
  * Finds the task with the closest deadline.
+ * This function takes a list of tasks and returns the one with the closest deadline.
  * @function getClosestDeadline
  * @param {Object[]} tasks - The list of tasks to check.
  * @returns {Object} - The task with the closest deadline.
@@ -167,6 +183,8 @@ function getClosestDeadline(tasks) {
 
 /**
  * Formats a date string to a more readable format.
+ * This function takes a date string and converts it to a more readable format (e.g., "January 1, 2024").
+ * If the date is invalid, it returns 'Invalid date'.
  * @function formatDeadline
  * @param {string} date - The date string to format.
  * @returns {string} - The formatted date string.
@@ -179,6 +197,7 @@ function formatDeadline(date) {
 
 /**
  * Redirects the user to the board page.
+ * This function redirects the user to the board page by updating the `window.location.href`.
  * @function loadBoard
  */
 function loadBoard() {
